4feaa702340c65871b43c709ea639db628351cff
Alexey <zoom-alex@yandex.ru>
2021-11-01 14:31:40 +0300

    Add script

---
+++ salary.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+salary, worked_hour, rate, benefit = argv
+
+print("Имя скрипта: ", salary)
+print("Параметр 1: ", worked_hour)
+print("Параметр 2: ", rate)
+print("Параметр 3: ", benefit)
