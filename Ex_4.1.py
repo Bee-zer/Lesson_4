4337d74b189eac1fb55ff82fa910563343251a58
Alexey <zoom-alex@yandex.ru>
2021-11-01 15:20:59 +0300

    Script correction

--- Ex_4.1.py
+++ Ex_4.1.py
@@ -2,5 +2,9 @@ from sys import argv
 
 salary, worked_hour, rate, benefit = argv
 
-calculation = (int(worked_hour) * int(rate)) + int(benefit)
-print(f"Your pay is equal {calculation}")
+print("Имя скрипта: ", salary)
+print("\n<< Программа рассчета заработной платы сотрудника >>")
+print("Выработка в часах: ", worked_hour)
+print("Ставка в час: ", rate)
+print("Премия: ", benefit)
+print("Зарплата сотрудника: ", (float(worked_hour) * float(rate)) + float(benefit))
